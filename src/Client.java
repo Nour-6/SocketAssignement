ab1dff353330c2223e0e28ebdf664178f72a4846
unknown <you@example.com>
2021-12-06 22:39:15 +0100

    last commit

---
+++ src/Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Client {
+
+    static final int port = 1200;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("connection request");
+
+        //Create a client socket
+        Socket socket = new Socket("127.0.0.1", port);
+        System.out.println("Connection established");
+
+        // create in / out
+        BufferedReader inClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter outClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        // todo: implement the scenario
+        Scanner scanner = new Scanner( System.in );
+        String ch = scanner.nextLine();
+        outClient.println(ch);
+        String ch1 =inClient.readLine();
+        System.out.println(ch1);
+        //close in / out
+        inClient.close();
+        outClient.close();
+
+        // close client socket
+        socket.close();
+    }
+}
